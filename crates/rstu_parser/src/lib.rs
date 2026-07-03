c1292bf4b7a3556f3629756294730d41ca6f823b
Jochen S. <76686336+joppi588@users.noreply.github.com>
2026-07-03 22:15:01 +0200

    Feature/173 basic testing (#3)
    * ast
    
    * copyright
    
    * sample lib
    
    * integration tests.
    
    ---------
    
    Co-authored-by: Jochen Schmähling <js1157@gmail.com>

---
+++ crates/rstu_parser/src/lib.rs
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2026 Jochen Schmaehling <tostmann1@web.de>
+//
+// SPDX-License-Identifier: MIT
+
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
