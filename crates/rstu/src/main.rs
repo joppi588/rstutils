6526f886b0ee2efe5d0969ec51fe675ef823ae24
Jochen Schmähling <76686336+joppi588@users.noreply.github.com>
2026-07-08 21:31:55 +0200

    basic cli tool (#10)
    Co-authored-by: Jochen Schmähling <{tostmann1@web.de}>

--- crates/rstu/src/main.rs
+++ crates/rstu/src/main.rs
@@ -1,3 +1,42 @@
+use clap::{Parser, Subcommand, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(name = "rstu")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Check {
+        file: String,
+    },
+    Format {
+        file: String,
+        #[arg(long)]
+        output: Option<OutputFormat>,
+    },
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum OutputFormat {
+    Json,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Check { file } => {
+            println!("Subcommand check, option file={file}");
+        }
+        Commands::Format { file, output } => {
+            let option_value = output
+                .map(|value| format!("{value:?}").to_lowercase())
+                .unwrap_or_else(|| "none".to_string());
+
+            println!("Subcommand format, option file={file}, output={option_value}");
+        }
+    }
 }
--- docs/index.md
+++ docs/index.md
@@ -5,3 +5,11 @@ Call using
 ```bash
 rstu <command> <file>
 ```
+
+Commands:
+
+```bash
+rstu check <file>
+rstu format <file>
+rstu format <file> --output json
+```
