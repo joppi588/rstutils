8c57e3253b9c68b14a299866f929710cbcea117e
Jochen Schmähling <{tostmann1@web.de}>
2026-07-21 18:46:36 +0000

    draft

--- crates/rstu_parser/src/token.rs
+++ crates/rstu_parser/src/token.rs
@@ -132,15 +132,15 @@ mod tests {
     #[test]
     fn section_title_prefix_matches() {
         assert!(TokenKind::SectionTitlePrefix.is_match("\n\n====\nTitle"));
-        assert!(!TokenKind::Transition.is_match("\n\n==a=\nTitle"));
-        assert!(!TokenKind::Transition.is_match("Title\n====\n"));
+        assert!(!TokenKind::SectionTitlePrefix.is_match("\n\n==a=\nTitle"));
+        assert!(!TokenKind::SectionTitlePrefix.is_match("Title\n====\n"));
     }
 
     #[test]
     fn section_title_suffix_matches() {
-        assert!(TokenKind::Transition.is_match("Title\n=====\nParagraph"));
-        assert!(!TokenKind::Transition.is_match("Title\n==a=\n\n"));
-        assert!(!TokenKind::Transition.is_match("\n====\nTitle"));
+        assert!(TokenKind::SectionTitleSuffix.is_match("Title\n=====\nParagraph"));
+        assert!(!TokenKind::SectionTitleSuffix.is_match("Title\n==a=\n\n"));
+        assert!(!TokenKind::SectionTitleSuffix.is_match("\n====\nTitle"));
     }
 
     #[test]
