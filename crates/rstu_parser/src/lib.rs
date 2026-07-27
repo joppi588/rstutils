67028134f88378f81ea0509f4cc3dbed7f43a925
Jochen Schmähling <{tostmann1@web.de}>
2026-07-27 20:45:17 +0000

    simplify.

--- crates/rstu_parser/src/lib.rs
+++ crates/rstu_parser/src/lib.rs
@@ -44,14 +44,14 @@ pub fn parse(input: &str) -> Result<NodeRef, FindElementError> {
         .expect("Token stream shall end with a newline.");
         match (tokens[index].kind, tokens[index_line_end + 1].kind) {
             (TokenKind::Separator, TokenKind::Indent) | (TokenKind::Separator, TokenKind::Word) => {
-                let (section, next_start) = try_match_section_header_prefix(&tokens, index)?;
+                let (section, next_start) = try_match_section_header(&tokens, index, true)?;
                 AstNode::push_section_ref(&current_node, section.clone())
                     .expect("Could not insert section!");
                 current_node = section;
                 index = next_start;
             }
             (TokenKind::Word, TokenKind::Separator) => {
-                let (section, index_end_header) = try_match_section_header_suffix(&tokens, index)?;
+                let (section, index_end_header) = try_match_section_header(&tokens, index, false)?;
                 AstNode::push_section_ref(&current_node, section.clone())
                     .expect("Could not insert section!");
                 current_node = section;
@@ -65,15 +65,18 @@ pub fn parse(input: &str) -> Result<NodeRef, FindElementError> {
     Ok(doc)
 }
 
-pub fn try_match_section_header_prefix(
-    tokens: &Vec<Token>,
+pub fn try_match_section_header(
+    tokens: &[Token],
     start_at: usize,
+    skip_prefix: bool,
 ) -> Result<(NodeRef, usize), FindElementError> {
-    let next_line_end = find_next_kind(
+    let title_start = start_at + usize::from(skip_prefix);
+    let line_search_start = if skip_prefix { start_at + 2 } else { start_at };
+    let line_end = find_next_kind(
         tokens,
         &[TokenKind::NewLine],
         ScanDirection::Forward,
-        start_at + 2,
+        line_search_start,
     )
     .map_err(
         |_| FindElementError::SectionTitleMissingClosingAfterOpening {
@@ -81,56 +84,33 @@ pub fn try_match_section_header_prefix(
         },
     )?;
 
-    let closing_index = next_line_end + 1;
+    let closing_index = line_end + 1;
     if (closing_index >= tokens.len()) || (tokens[closing_index].kind != TokenKind::Separator) {
         return Err(FindElementError::SectionTitleMissingClosingAfterOpening {
             opening_index: start_at,
         });
     }
 
-    let opening_style = tokens[start_at].lexeme.clone(); // TODO: single char + opening/closing length
     let closing_style = tokens[closing_index].lexeme.clone();
-    if tokens[start_at].lexeme != tokens[closing_index].lexeme {
-        return Err(FindElementError::SectionTitleUnbalancedStyle {
-            opening_index: start_at,
-            opening_style: opening_style,
-            closing_style: closing_style,
-        });
+
+    if skip_prefix {
+        let opening_style = tokens[start_at].lexeme.clone(); // TODO: single char + opening/closing length
+        if tokens[start_at].lexeme != tokens[closing_index].lexeme {
+            return Err(FindElementError::SectionTitleUnbalancedStyle {
+                opening_index: start_at,
+                opening_style,
+                closing_style: closing_style.clone(),
+            });
+        }
     }
+
     let section_marker = AstNode::new_ref(ElementKind::Section);
     AstNode::with_attr(&section_marker, "section_marker", closing_style);
 
     let title = AstNode::new_ref(ElementKind::Title);
-    AstNode::with_text(
-        &title,
-        tokens_to_text(&tokens[start_at + 1..next_line_end + 1]),
-    );
+    AstNode::with_text(&title, tokens_to_text(&tokens[title_start..line_end + 1]));
     AstNode::push_child(&section_marker, title)
         .expect("section title should always be a valid section child");
 
     Ok((section_marker, closing_index + 1))
-}
-
-pub fn try_match_section_header_suffix(
-    tokens: &Vec<Token>,
-    start_at: usize,
-) -> Result<(NodeRef, usize), FindElementError> {
-    let line_end = find_next_kind(
-        tokens,
-        &[TokenKind::NewLine],
-        ScanDirection::Forward,
-        start_at,
-    )
-    .expect("Section title ends with new line.");
-    let closing_style = tokens[line_end + 1].lexeme.clone();
-
-    let section_marker = AstNode::new_ref(ElementKind::Section);
-    AstNode::with_attr(&section_marker, "section_marker", closing_style);
-
-    let title = AstNode::new_ref(ElementKind::Title);
-    AstNode::with_text(&title, tokens_to_text(&tokens[start_at..line_end + 1]));
-    AstNode::push_child(&section_marker, title)
-        .expect("section title should always be a valid section child");
-
-    Ok((section_marker, line_end + 2))
 }
