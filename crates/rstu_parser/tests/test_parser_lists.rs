a7a041d4bfa8948f2d7b07a4f38e5b73920d7d5e
Jochen Schmähling <{tostmann1@web.de}>
2026-08-03 14:23:20 +0000

    field list tests.

---
+++ crates/rstu_parser/tests/test_parser_lists.rs
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2026 Jochen Schmaehling <tostmann1@web.de>
+//
+// SPDX-License-Identifier: MIT
+
+use rstest::rstest;
+use rstu_ast::AstNode;
+use rstu_parser::parse;
+use std::fs;
+use std::path::Path;
+
+mod test_parser;
+
+#[rstest]
+#[case("lists", "ok_field_list.rst", "ok_field_list.yaml")]
+fn parses_directives_and_matches_yaml_fixture(
+    #[case] directory: &str,
+    #[case] rst_filename: &str,
+    #[case] yaml_filename: &str,
+) {
+    rst_vs_yaml!(directory, rst_filename, yaml_filename)
+}
