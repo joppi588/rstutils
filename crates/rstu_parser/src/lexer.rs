d306fabdf033ec2797ded0044a3dd0ee05082b57
Jochen Schmähling <{tostmann1@web.de}>
2026-08-10 05:41:20 +0000

    TK

--- crates/rstu_parser/src/lexer.rs
+++ crates/rstu_parser/src/lexer.rs
@@ -2,40 +2,38 @@
 //
 // SPDX-License-Identifier: MIT
 
-use crate::token::{Token, TokenKind};
+use crate::token::{Token, TokenKind as TK};
 
 pub fn tokenize(input: &str) -> Vec<Token> {
     let input = format!("\n\n{input}\n\n"); // leading and trailing blank line
     let mut tokens: Vec<Token> = Vec::new();
-    let mut last_token_kind = TokenKind::BlankLine;
+    let mut last_token_kind = TK::BlankLine;
     let mut current_indent = 0;
 
     let mut index: usize = 1;
     while index < input.len() - 1 {
         let sub_str = &input[index - 1..];
-        let (token_kind, lexeme) = TokenKind::match_token(sub_str)
+        let (token_kind, lexeme) = TK::match_token(sub_str)
             .unwrap_or_else(|| panic!("No token matched input: {sub_str:?}"));
         let new_token = Token::new(token_kind, lexeme);
         match (last_token_kind, token_kind) {
-            (TokenKind::NewLine | TokenKind::BlankLine, TokenKind::Indent) => {
+            (TK::NewLine | TK::BlankLine, TK::Indent) => {
                 let new_indent = lexeme.len();
                 if new_indent > current_indent {
                     let indent_token =
-                        Token::new(TokenKind::Indent, " ".repeat(new_indent - current_indent));
+                        Token::new(TK::Indent, " ".repeat(new_indent - current_indent));
                     tokens.push(indent_token);
                 } else if new_indent < current_indent {
                     let dedent_token =
-                        Token::new(TokenKind::Dedent, " ".repeat(current_indent - new_indent));
+                        Token::new(TK::Dedent, " ".repeat(current_indent - new_indent));
                     tokens.push(dedent_token);
                 }
                 current_indent = new_indent;
             }
-            (TokenKind::NewLine | TokenKind::BlankLine, TokenKind::BlankLine) => {
-                tokens.push(new_token)
-            } // Blank line does not change indent
-            (TokenKind::NewLine | TokenKind::BlankLine, _) => {
+            (TK::NewLine | TK::BlankLine, TK::BlankLine) => tokens.push(new_token), // Blank line does not change indent
+            (TK::NewLine | TK::BlankLine, _) => {
                 if current_indent > 0 {
-                    let dedent_token = Token::new(TokenKind::Dedent, " ".repeat(current_indent));
+                    let dedent_token = Token::new(TK::Dedent, " ".repeat(current_indent));
                     tokens.push(dedent_token);
                 }
                 current_indent = 0;
@@ -48,7 +46,7 @@ pub fn tokenize(input: &str) -> Vec<Token> {
         index += lexeme.len();
     }
     if current_indent > 0 {
-        tokens.push(Token::new(TokenKind::Dedent, " ".repeat(current_indent)))
+        tokens.push(Token::new(TK::Dedent, " ".repeat(current_indent)))
     };
     tokens
 }
@@ -62,12 +60,12 @@ mod tests {
     fn tokenize_spaces_words() {
         let input = "Hello World\n";
         let expected = vec![
-            Token::new(TokenKind::BlankLine, "\n"),
-            Token::new(TokenKind::Word, "Hello"),
-            Token::new(TokenKind::Spaces, " "),
-            Token::new(TokenKind::Word, "World"),
-            Token::new(TokenKind::NewLine, "\n"),
-            Token::new(TokenKind::BlankLine, "\n"),
+            Token::new(TK::BlankLine, "\n"),
+            Token::new(TK::Word, "Hello"),
+            Token::new(TK::Spaces, " "),
+            Token::new(TK::Word, "World"),
+            Token::new(TK::NewLine, "\n"),
+            Token::new(TK::BlankLine, "\n"),
         ];
 
         assert_eq!(tokenize(input), expected);
@@ -77,12 +75,12 @@ mod tests {
     fn tokenize_treats_unmatched_input_as_literal_string() {
         let input = "abc\x07def\n";
         let expected = vec![
-            Token::new(TokenKind::BlankLine, "\n"),
-            Token::new(TokenKind::Word, "abc"),
-            Token::new(TokenKind::LiteralChar, "\x07"),
-            Token::new(TokenKind::Word, "def"),
-            Token::new(TokenKind::NewLine, "\n"),
-            Token::new(TokenKind::BlankLine, "\n"),
+            Token::new(TK::BlankLine, "\n"),
+            Token::new(TK::Word, "abc"),
+            Token::new(TK::LiteralChar, "\x07"),
+            Token::new(TK::Word, "def"),
+            Token::new(TK::NewLine, "\n"),
+            Token::new(TK::BlankLine, "\n"),
         ];
 
         assert_eq!(tokenize(input), expected);
@@ -100,17 +98,17 @@ mod tests {
         assert_eq!(
             actual,
             vec![
-                (TokenKind::BlankLine, "\n"),
-                (TokenKind::Word, "line_1"),
-                (TokenKind::NewLine, "\n"),
-                (TokenKind::Indent, "    "),
-                (TokenKind::Word, "nested"),
-                (TokenKind::NewLine, "\n"),
-                (TokenKind::Dedent, "  "),
-                (TokenKind::Word, "dedented"),
-                (TokenKind::NewLine, "\n"),
-                (TokenKind::BlankLine, "\n"),
-                (TokenKind::Dedent, "  ")
+                (TK::BlankLine, "\n"),
+                (TK::Word, "line_1"),
+                (TK::NewLine, "\n"),
+                (TK::Indent, "    "),
+                (TK::Word, "nested"),
+                (TK::NewLine, "\n"),
+                (TK::Dedent, "  "),
+                (TK::Word, "dedented"),
+                (TK::NewLine, "\n"),
+                (TK::BlankLine, "\n"),
+                (TK::Dedent, "  ")
             ]
         );
     }
@@ -127,16 +125,16 @@ mod tests {
         assert_eq!(
             actual,
             vec![
-                (TokenKind::BlankLine, "\n"),
-                (TokenKind::Word, "line_1"),
-                (TokenKind::NewLine, "\n"),
-                (TokenKind::Indent, "    "),
-                (TokenKind::Word, "nested"),
-                (TokenKind::NewLine, "\n"),
-                (TokenKind::Dedent, "    "),
-                (TokenKind::Word, "plain"),
-                (TokenKind::NewLine, "\n"),
-                (TokenKind::BlankLine, "\n"),
+                (TK::BlankLine, "\n"),
+                (TK::Word, "line_1"),
+                (TK::NewLine, "\n"),
+                (TK::Indent, "    "),
+                (TK::Word, "nested"),
+                (TK::NewLine, "\n"),
+                (TK::Dedent, "    "),
+                (TK::Word, "plain"),
+                (TK::NewLine, "\n"),
+                (TK::BlankLine, "\n"),
             ]
         );
     }
