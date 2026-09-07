efd2ca2caba846664f36e6d37c9b489e50a35c9c
Jochen Schmähling <{tostmann1@web.de}>
2026-09-07 19:12:39 +0000

    remove directories

--- crates/rstu_parser/tests/test_parser_directives.rs
+++ crates/rstu_parser/tests/test_parser_directives.rs
@@ -11,10 +11,10 @@ use std::path::Path;
 mod test_parser;
 
 #[rstest]
-#[case("directives", "ok_note_simple")]
-#[case("directives", "ok_comment")]
-#[case("directives", "ok_image_numeric_options")]
-#[case("directives", "ok_image_options_and_content")]
-fn parses_directives_and_matches_yaml_fixture(#[case] directory: &str, #[case] test_case: &str) {
-    rst_vs_yaml!(directory, test_case)
+#[case("ok_note_simple")]
+#[case("ok_comment")]
+#[case("ok_image_numeric_options")]
+#[case("ok_image_options_and_content")]
+fn parses_directives_and_matches_yaml_fixture(#[case] test_case: &str) {
+    rst_vs_yaml!("directives", test_case)
 }
--- crates/rstu_parser/tests/test_parser_inline.rs
+++ crates/rstu_parser/tests/test_parser_inline.rs
@@ -10,8 +10,8 @@ use std::fs;
 use std::path::Path;
 
 #[rstest]
-#[case("inline", "ok_strong")]
-#[case("inline", "ok_inline_variants")]
-fn parse_inline(#[case] directory: &str, #[case] test_case: &str) {
-    rst_vs_yaml!(directory, test_case)
+#[case("ok_strong")]
+#[case("ok_inline_variants")]
+fn parse_inline(#[case] test_case: &str) {
+    rst_vs_yaml!("inline", test_case)
 }
