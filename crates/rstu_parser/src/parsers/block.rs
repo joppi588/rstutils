af55170d4c762de78c53c37605ea188411d04cf9
Jochen Schmähling <{tostmann1@web.de}>
2026-09-09 18:52:22 +0000

    block

--- crates/rstu_parser/src/parsers/block.rs
+++ crates/rstu_parser/src/parsers/block.rs
@@ -21,8 +21,7 @@ pub(crate) fn parse_block(
         TK::Field | TK::BulletListMarker => {
             parse_paragraph(tokens, start_at, Some(following_index), None)
         }
-        TK::Dedent => parse_compound_block(tokens, start_at, start_at - 1),
-        _ => Err(ParserError::UnexpectedBlockEndError {}),
+        _ => parse_compound_block(tokens, start_at, start_at - 1),
     }
 }
 
