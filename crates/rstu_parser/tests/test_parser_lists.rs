aebba8a768b2a005d467819e4556cff9accc3fda
Jochen Schmähling <{tostmann1@web.de}>
2026-09-11 04:59:45 +0000

    fix remaining test cases.

--- crates/rstu_parser/tests/test_parser_lists.rs
+++ crates/rstu_parser/tests/test_parser_lists.rs
@@ -16,8 +16,8 @@ mod test_parser;
 #[case("ok_compact_bullet_list")]
 #[case("docutils_bullet_00")]
 #[case("docutils_bullet_01")]
+#[case("docutils_bullet_02")]
 #[case("docutils_bullet_03")]
-// #[case("docutils_bullet_02")] no blank lines
 // #[case("docutils_bullet_04")]
 #[case("docutils_bullet_07")]
 // #[case("docutils_bullet_09")]
