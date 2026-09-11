392146cace1fce794883599d1abcad6bd73889e9
Jochen Schmähling <{tostmann1@web.de}>
2026-09-11 13:52:42 +0000

    comment

--- crates/rstu_parser/src/lib.rs
+++ crates/rstu_parser/src/lib.rs
@@ -27,7 +27,7 @@ pub fn parse(input: &str) -> Result<NodeRef, ParserError> {
     let mut current_node = doc.clone();
 
     while index < tokens.len() - 2 {
-        // -2: final two tokens are always NewLine+Blankline(TODO: tbc)
+        // final two tokens are always NewLine+Blankline
         let index_line_end = find_next_kind(&tokens, &[TK::NewLine], index, None)
             .expect("Token stream ends with a newline."); // TODO: Integrate this in token stream.
         match (tokens[index].kind, tokens[index_line_end + 1].kind) {
