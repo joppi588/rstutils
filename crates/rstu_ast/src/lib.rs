a443e31a5bed17510ea5f17f6d7b330007332a98
Jochen Schmähling <{tostmann1@web.de}>
2026-09-11 13:35:33 +0000

    remove dead code

--- crates/rstu_ast/src/lib.rs
+++ crates/rstu_ast/src/lib.rs
@@ -51,20 +51,13 @@ impl NodeRefExt for NodeRef {
     }
 
     fn push_body_element(&self, body: NodeRef) -> NodeRef {
-        let class = self.borrow().class;
-        match class {
-            NodeClass::Document | NodeClass::Section => self.push_child(body.clone()),
-            _ => {
-                let parent = self
-                    .borrow()
-                    .parent
-                    .as_ref()
-                    .and_then(Weak::upgrade)
-                    .unwrap_or_else(|| self.clone()); // TODO: what does self.clone? Is that branch possible?
-                parent.push_child(body.clone());
-            }
-        }
-
+        let parent = self
+            .borrow()
+            .parent
+            .as_ref()
+            .and_then(Weak::upgrade)
+            .unwrap_or_else(|| self.clone()); // TODO: what does self.clone? Is that branch possible?
+        parent.push_child(body.clone());
         body
     }
 
