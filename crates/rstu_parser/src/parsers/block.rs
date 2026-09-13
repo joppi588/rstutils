9dce0745cf3e4e7b3114decdf2328ac76eb90d74
Jochen Schmähling <{tostmann1@web.de}>
2026-09-13 08:29:07 +0000

    skip blank lines.

--- crates/rstu_parser/src/parsers/block.rs
+++ crates/rstu_parser/src/parsers/block.rs
@@ -52,14 +52,10 @@ pub(crate) fn parse_block_hanging_indent(
     let block = AstNode::new_ref(NodeClass::Block);
     let mut indent: Option<usize> = None;
     let mut index = start_at;
-    let mut index_line_end = find_next_kind(&tokens, &[TK::NewLine], index, None)
+    let index_line_end = find_next_kind(&tokens, &[TK::NewLine], index, None)
         .expect("Token stream ends with a newline."); // TODO: Integrate this in token stream.
 
     // Parse the first paragraph
-    while index_line_end < tokens.len() - 2 && tokens[index_line_end + 2].kind == TK::BlankLine {
-        block.push_child(AstNode::new_ref(NodeClass::BlankLine));
-        index_line_end += 1;
-    }
 
     match tokens[index_line_end + 1].kind {
         TK::BlankLine | TK::Field | TK::BulletListMarker => {
@@ -77,6 +73,11 @@ pub(crate) fn parse_block_hanging_indent(
             index = new_index;
         }
         _ => return Err(ParserError::UnexpectedBlockEndError {}),
+    }
+
+    while index < tokens.len() - 1 && tokens[index + 1].kind == TK::BlankLine {
+        block.push_child(AstNode::new_ref(NodeClass::BlankLine));
+        index += 1;
     }
 
     // Parse the rest
