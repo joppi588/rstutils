1ae0236e8a91a3491a8d2a878f4b116bf9ec64c7
Jochen Schmähling <76686336+joppi588@users.noreply.github.com>
2026-09-16 21:43:13 +0200

    dont skip blanklines (#91)
    * dont skip blanklines
    
    * comment.
    
    ---------
    
    Co-authored-by: Jochen Schmähling <{tostmann1@web.de}>

--- crates/rstu_parser/src/lib.rs
+++ crates/rstu_parser/src/lib.rs
@@ -63,11 +63,12 @@ pub fn parse(input: &str) -> Result<NodeRef, ParserError> {
                 index = next_start;
             }
 
-            // TODO: Do not simply ignore these, especially the blank lines
-            (TK::NewLine, TK::BlankLine)
-            | (TK::BlankLine, _)
-            | (TK::Indent, _)
-            | (TK::Dedent, _) => index += 1,
+            (TK::BlankLine, _) => {
+                current_parent.push_blank_lines(tokens[index].lexeme.len());
+                index += 1;
+            }
+            // TODO: Do not simply ignore these
+            (TK::Indent, _) | (TK::Dedent, _) => index += 1,
 
             (kind, _)
                 if kind.is(TC::INLINE_MARKER)
