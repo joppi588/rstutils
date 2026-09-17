052b08c726ec214b5bc56bc60f470e5df1ee2606
Jochen Schmähling <{tostmann1@web.de}>
2026-09-17 05:24:37 +0000

    blank lines

--- crates/rstu_parser/src/lib.rs
+++ crates/rstu_parser/src/lib.rs
@@ -63,8 +63,12 @@ pub fn parse(input: &str) -> Result<NodeRef, ParserError> {
                 index = next_start;
             }
 
+            (TK::BlankLine, _) => {
+                current_parent.push_blank_lines(tokens[index].lexeme.len());
+                index += 1;
+            }
             // TODO: Do not simply ignore these
-            (TK::BlankLine, _) | (TK::Indent, _) | (TK::Dedent, _) => index += 1,
+            (TK::Indent, _) | (TK::Dedent, _) => index += 1,
 
             (kind, _)
                 if kind.is(TC::INLINE_MARKER)
